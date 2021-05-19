afc09a8318aa733ed6a6aa7f5d55f24741521e37
Sahrial Ardians <sahrialardians@gmail.com>
2021-05-20 04:39:19 +0700

    delete section header layout apps

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -30,10 +30,6 @@
 
       <!-- Main Content -->
       <div class="main-content">
-        <section class="section">
-          <div class="section-header">
-            <h1>Dashboard</h1>
-          </div>
 
           @yield('content')
 
