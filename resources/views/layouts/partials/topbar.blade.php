db92a9c113c0f3c1c52960bd311ec320cb6ba508
Sahrial Ardians <sahrialardians@gmail.com>
2021-05-24 00:06:30 +0700

    clear link to html

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -7,13 +7,13 @@
     <ul class="navbar-nav navbar-right">
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
         <img alt="image" src="{{ asset('backend/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+        <div class="d-sm-none d-lg-inline-block">Halo, {{ Auth::user()->name }}</div></a>
         <div class="dropdown-menu dropdown-menu-right">
-          <div class="dropdown-title">Logged in 5 min ago</div>
-          <a href="features-profile.html" class="dropdown-item has-icon">
+          {{-- <div class="dropdown-title">Logged in 5 min ago</div> --}}
+          <a href="#" class="dropdown-item has-icon">
             <i class="far fa-user"></i> Profile
           </a>
-          <a href="features-settings.html" class="dropdown-item has-icon">
+          <a href="#" class="dropdown-item has-icon">
             <i class="fas fa-cog"></i> Settings
           </a>
           <div class="dropdown-divider"></div>
