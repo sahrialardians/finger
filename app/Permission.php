acc70a0a412f1d63276edff2879a5b7b6b30fd16
Sahrial Ardians <sahrialardians@gmail.com>
2021-06-12 09:11:33 +0700

    change extend to bawaan model Permission spatie

--- app/Permission.php
+++ app/Permission.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Permission extends Model
+class Permission extends \Spatie\Permission\Models\Permission
 {
     protected $fillable = ['name', 'guard_name'];
 
