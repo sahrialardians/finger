878c1464e9f1fad5686314eeefe56a43f54a74b3
Sahrial Ardians <sahrialardians@gmail.com>
2021-06-12 09:10:28 +0700

    change extend to bawaan model Role

--- app/Role.php
+++ app/Role.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+class Role extends \Spatie\Permission\Models\Role
 {
     protected $fillable = ['name', 'guard_name'];
 }
