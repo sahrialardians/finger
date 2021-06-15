463505e61a7ba52542f5e0ec8e549a984316bf5f
Sahrial Ardians <sahrialardians@gmail.com>
2021-06-15 08:41:04 +0700

    create model & migration device

---
+++ database/migrations/2021_06_15_005315_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->integer('device_number');
+            $table->string('type_of_communication', 100);
+            $table->string('ip_address', 100);
+            $table->integer('port');
+            $table->text('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
